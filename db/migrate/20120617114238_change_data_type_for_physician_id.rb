ae13d77d0bceb0405147c3c538bf1526719151e1
brijesh shetty <brij.jhu@gmail.com>
2012-06-17 08:03:39 -0400

    fix physician_id's type

--- db/migrate/20120617114238_change_data_type_for_physician_id.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataTypeForPhysicianId < ActiveRecord::Migration
-  def up
-  	change_column :patients, :physician_id, :integer
-  end
-
-  def down
-	change_column :patients, :physician_id, :string
-  end
-end
